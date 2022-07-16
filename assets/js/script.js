41611e69622418fc0a5bccf656c86c6e135b9905
jcuetos97 <jcuetos97@gmail.com>
2022-07-16 16:51:15 -0500

    script.js update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-addEventListener("click", darkmode()); 
+
+
+var homebttn = $('#homebttn');
+
+var isDark = true;
+
+homebttn.on('click', function () {
+//   if (isDark) {
+//     $('body').css({ 'background-color': '#d9e9e8', color: '#1a1a1a' });
+//     isDark = !isDark;
+    console.log('body');
+//   } else {
+//     $('body').css({ 'background-color': '#1a1a1a', color: '#d9e9e8' });
+//     isDark = !isDark;
+  });
+// });
