f1ed38e521ce2331cfbfea630f527dc4418a4e91
jcuetos97 <jcuetos97@gmail.com>
2022-08-08 18:21:07 -0500

    my-work.html update

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,20 +7,22 @@ var isDark = false;
 
 homebttn.on('click', function () {
    if (isDark) {
-    $('body').css({ 'background-color': '#d9e9e8', color: '#1a1a1a' });
-    $('#githublogo').css({'filter': "none"});
-    $('.dropbtn').css({'filter': "none"});
-    $(this).find('i').attr("class", "fa-regular fa-sun fa-2xs"); 
-    isDark = !isDark;
-   
-   } else {
     $('body').css({ 'background-color': '#1a1a1a', color: '#d9e9e8' }); 
     $('#githublogo').css({'filter': "brightness(0%)", "filter":"invert(1)"});
     $('.dropbtn').css({'filter': "brightness(0%)", "filter":"invert(1)"});
     $(this).find('i').attr("class", "fa-regular fa-moon fa-2xs");
+    isDark = !isDark;
+   
+   } else {
+    $('body').css({ 'background-color': '#d9e9e8', color: '#1a1a1a' });
+    $('#githublogo').css({'filter': "none"});
+    $('.dropbtn').css({'filter': "none"});
+    $(this).find('i').attr("class", "fa-regular fa-sun fa-2xs"); 
      isDark = !isDark;
     }
  });
+
+
 
  contact.on('click', function () {
   
